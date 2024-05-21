ddec0636cfa6f36eb4f9ddde66266fb2066054d3
yorenvvl <12001808@student.pxl.be>
2024-05-21 13:58:08 +0200

    listgroup

---
+++ src/Components/ListGroup.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+//(items: [], heading: string)
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, SetSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No itmes found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              SetSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing
+function Message(){
+    //JSX = Javascript XML
+    const name = "Yoren";
+    if (name)
+        return <h1>Hello {name}!</h1>;
+    return <h1>Hello world!</h1>
+}
+
+export default Message;
